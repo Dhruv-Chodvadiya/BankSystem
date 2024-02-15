d2678e1caa5ba0fdb8f968295d2e24fcff419473
Dhruv <chodvadiyadhruv06@gmail.com>
2024-02-15 22:02:34 +0530

    Changes

--- src/main/java/ASS2/BankSystem/BankServices/TransationSrevice.java
+++ src/main/java/ASS2/BankSystem/BankServices/TransationSrevice.java
@@ -10,10 +10,8 @@ import java.util.List;
 
 @Service
 public class TransationSrevice {
-
     @Autowired
     TransationRepo transationRepo;
-
     public List<TransationStatment> statment(String acNumber) {
         return transationRepo.findByAcNumber(acNumber);
     }
